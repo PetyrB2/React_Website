35779cd5692f4b6d0adc523634d21a6469c54e75
PetyrB2 <peter.i.broadbent@gmail.com>
2022-06-21 15:14:39 +0100

    Got the Nav working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <div className="App">
+      <nav>
+        <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/About'>About</Link>
+        </li>
+        <li>
+          <Link to='/Contact'>Contact</Link>
+        </li>
+        </ul>
+      </nav>
+    
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/About' element={<About />} />
+      <Route path='/Contact' element={<Contact />} />
+      </Routes>
+      
+      </div>
+
+    </Router>
   );
 }
 
